1783e2c2cd70d0405dc9a11c3bcdffc0fd9d0544
ericyang0329 <ericyang0329@gmail.com>
2018-03-20 17:13:03 +0800

    [query_imgurl.bq.sql] dedupe with group by

--- utils/query_imgurl.bq.sql
+++ utils/query_imgurl.bq.sql
@@ -6,11 +6,18 @@ WITH
     {cn}_Unima.GoodsCategoryFlatten_{dt}
   WHERE
     category_code = '{ccode}')
+
 SELECT 
+  goods_img_url 
+FROM (
+    SELECT 
+      goods_img_url
+    FROM
+      {cn}_Unima.Goods_{dt} AS t_goods
+    INNER JOIN
+      t_cc
+    ON
+      t_goods.gid = t_cc.gid
+)
+GROUP BY
   goods_img_url
-FROM
-  {cn}_Unima.Goods_{dt} AS t_goods
-INNER JOIN
-  t_cc
-ON
-  t_goods.gid = t_cc.gid
