20a519a1bd6d976e231a7318d63c7cd1bbae34bd
ericyang0329 <ericyang0329@gmail.com>
2018-03-20 15:20:31 +0800

    [urils] image downloader

---
+++ utils/img_downloader.py
@@ -0,0 +1,47 @@
+import os
+import argparse
+import subprocess
+from urllib import urlretrieve
+
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-cn', required=True, help='codename')
+    parser.add_argument('-dt', required=True, help='datetime, e.g. 20180222')
+    parser.add_argument('-cc', required=True, help='category code')
+    args = parser.parse_args()
+
+    sql = ''
+    with open('query_imgurl.bq.sql', 'r') as f:
+        sql = f.read()
+    sql = sql.format(cn=args.cn, dt=args.dt, ccode=args.cc)
+
+    name = 'imgurls'
+    tmpTb = '{cn}_tmp.{tb}'.format(cn=args.cn, tb=name)
+    cmd = 'bq query -n 0 --replace --use_legacy_sql=False --destination_table={} {}'.format(tmpTb, sql)
+    print cmd
+#    subprocess.call(cmd.split(' '))
+
+    gsTmpFP = os.path.join('gs://ven-cust-{cn}/tmp/{fn}.tsv'.format(cn=args.cn, fn=name))
+    cmd = 'bq extract --noprint_header -F ''\t'' {} {}'.format(tmpTb, gsTmpFP)
+    print cmd
+    subprocess.call(cmd.split(' '))
+
+    cmd = 'sudo gsutil cp {} {}'.format(gsTmpFP, '.')
+    print cmd
+    subprocess.call(cmd.split(' '))
+
+    imgDir = 'image'
+    cmd = 'rm -rf {}'.format(imgDir)
+    print cmd
+    subprocess.call(cmd.split(' '))
+    os.makedirs(imgDir)
+
+    with open('{}.tsv'.format(name), 'r') as f:
+        for url in f:
+            url = url.rstrip()
+            h, t = os.path.split(url)
+            if t:
+                print 'download {}...'.format(url)
+                urlretrieve(url, os.path.join(imgDir, t))
---
+++ utils/query_imgurl.bq.sql
@@ -0,0 +1,16 @@
+WITH
+  t_cc AS (
+  SELECT
+    gid
+  FROM
+    {cn}_Unima.GoodsCategoryFlatten_{dt}
+  WHERE
+    category_code = '{ccode}')
+SELECT 
+  goods_img_url
+FROM
+  {cn}_Unima.Goods_{dt} AS t_goods
+INNER JOIN
+  t_cc
+ON
+  t_goods.gid = t_cc.gid
