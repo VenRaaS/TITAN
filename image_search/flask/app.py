a9812f66ef7aa66ae5c66894cc1bf3b17e07f7d9
ericyang0329 <ericyang0329@gmail.com>
2018-04-12 19:45:09 +0800

    [i2i.html] list image i2i list from pre-built model

--- image_search/flask/app.py
+++ image_search/flask/app.py
@@ -6,6 +6,7 @@ from flask import Flask, render_template, request, send_from_directory
 from werkzeug import secure_filename
 
 from sim_imgs_flat_model import search_sim_images
+import i2iList
 
 
 app = Flask(__name__)
@@ -30,8 +31,15 @@ def uploader() :
         f.save(fpath)
        
         imgFNs = search_sim_images(DIR_FEAVCT_MODEL, fpath)
-#--DEBUG        imgFNs = ['1001369_L.jpg', '1006938_L.jpg', '1007180_L.jpg'] 
         return render_template('sim_images.html', uploadFN=fn, localFNs=imgFNs)
+
+@app.route('/i2i/<filename>')
+def i2i(filename) :
+#--DEBUG        imgFNs = ['1001369_L.jpg', '1006938_L.jpg', '1007180_L.jpg'] 
+    inBasename = os.path.splitext(filename)[0]
+    imgFNs = i2iList.i2iList(inBasename, 'FeaVCT_cc21000_flat/image_i2i.tsv')
+    imgFNs = [ bn + '.jpg' for bn in imgFNs ]
+    return render_template('i2i.html', curFN=filename, localFNs=imgFNs)
 
 @app.route('/sfd_uploads/<filename>')
 def send_upload_file(filename):
