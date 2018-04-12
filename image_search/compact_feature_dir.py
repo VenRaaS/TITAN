9c6be678818ed7eb2d22eb9b5b4d7097bb8f89d0
ericyang0329 <ericyang0329@gmail.com>
2018-04-12 11:06:42 +0800

    [compact_feature_dir.py] updated - compact all feature vectores in a file instead of one feature vector per image

--- image_search/compact_feature_dir.py
+++ image_search/compact_feature_dir.py
@@ -2,45 +2,50 @@ import os
 import sys
 import argparse
 import json
-from multiprocessing import Pool
+from multiprocessing import Pool, Manager
 
 import numpy as np
 
 
-
+imgBN2Fea1D_dic = Manager().dict()
 def compact_feas_dir(path) :
     imgFea_list = []
     imgBN_list = []
 
+    print '{} is processing...'.format(path)
     for fn in os.listdir(path):
         if fn.endswith('.fea.npy'):
             fp = os.path.join(path, fn)
 #            print fp
             imgFea = np.load(fp)
-            imgFea_list.append( imgFea )
-            imgBN_list.append( fn.split('.')[0] )
-
-    fea_na = np.asarray(imgFea_list)
-    bn_na = np.asarray(imgBN_list)
-
-    print '{} is processing...'.format(path)
-    if 0 < fea_na.size and 0 < bn_na.size:
-        dirname = os.path.split(path)[-1]
-        np.save(os.path.join(path, dirname + '.feas'), fea_na)
-        np.save(os.path.join(path, dirname + '.bns'), bn_na)
+            imgBasename = fn.split('.')[0]
+            imgBN2Fea1D_dic[imgBasename] = imgFea
 
 
 if '__main__' == __name__:
     parser = argparse.ArgumentParser()
     parser.add_argument("dir", help="the root dir of all image features")
     args = parser.parse_args()
-    
-    if not os.path.isdir(args.dir):
-        print 'an invalid dir: {}'.format(args.dir)
+   
+    modelDir = os.path.normpath(args.dir) 
+    if not os.path.isdir(modelDir):
+        print 'an invalid dir: {}'.format(modelDir)
         sys.exit(1)
 
-    paths = [ root for root, dirs, files in os.walk(args.dir) ]
+    paths = [ root for root, dirs, files in os.walk(modelDir) ]
+
     pool = Pool(processes=100)
     pool.map(compact_feas_dir, paths)
 
- 
+    print len(imgBN2Fea1D_dic)
+
+    fea_na = np.asarray(imgBN2Fea1D_dic.values())
+    bn_na = np.asarray(imgBN2Fea1D_dic.keys())
+
+    if 0 < fea_na.size and 0 < bn_na.size:
+        print os.path.join(modelDir, modelDir + '.feas')
+        np.save(os.path.join(modelDir, modelDir + '.feas'), fea_na)
+
+        print os.path.join(modelDir, modelDir + '.bns')
+        np.save(os.path.join(modelDir, modelDir + '.bns'), bn_na)
+
