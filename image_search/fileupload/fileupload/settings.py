167c40c41efbdeec588f4141e89b0a42059965dc
ericyang0329 <ericyang0329@gmail.com>
2018-06-15 11:57:55 +0800

    [cors access-control-allow-origin] - upload_app/middleware/*, middleware response - settings.py, config middleware

--- image_search/fileupload/fileupload/settings.py
+++ image_search/fileupload/fileupload/settings.py
@@ -49,6 +49,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'upload_app.middleware.corsMiddleware',
 ]
 
 ROOT_URLCONF = 'fileupload.urls'
---
+++ image_search/fileupload/upload_app/middleware/__init__.py
@@ -0,0 +1,2 @@
+from corsMiddleware import corsMiddleware
+
